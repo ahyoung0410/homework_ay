11c1b30f84a1d93d79bc52ab463540476448bc70
ahyoung0410 <ayoung1283@daum.net>
2023-04-17 14:15:12 +0900

    assignment 3

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,10 +8,14 @@ export default function FirstPost() {
       <h4>
         Hi, My name is Ah-young and my major is Business Administration. My hobby is watching Netflix. Thank you.
       </h4>
+      
+      <iframe src="/processing.html" title="Processing Example" width="600" height="400"></iframe>
 
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+
+     
     </>
   );
 }
