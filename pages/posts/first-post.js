64ec86e97ff610861f6a2fd8318e21c0943c94a2
ahyoung0410 <ayoung1283@daum.net>
2023-04-18 10:52:40 +0900

    assignment 3

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+//processing.html파일과 연관파일(next.config.js, processing-1.4.1.js)들은 public폴더에 위치함.
+
 export default function FirstPost() {
   return (
     <>
@@ -9,7 +11,9 @@ export default function FirstPost() {
         Hi, My name is Ah-young and my major is Business Administration. My hobby is watching Netflix. Thank you.
       </h4>
       
-      <iframe src="/processing.html" title="Processing Example" width="600" height="400"></iframe>
+    
+      <iframe src="/processing.html" title="Bouncing-ball" width="600" height="400"></iframe>
+    
 
       <h2>
         <Link href="/">Back to home</Link>
